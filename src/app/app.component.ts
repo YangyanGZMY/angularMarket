d0ca43cc9b9560bcf7480ffa5f9ef81e588aef2b
zhangmingyang <zhangmingyang@thunisoft.com>
2019-10-09 13:17:33 +0800

    create angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angularMarket123';
+}
