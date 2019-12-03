3c1d1595432a9cc44f14fe02b424bf46bff35fcf
zhangmingyang <zhangmingyang@thunisoft.com>
2019-12-03 15:21:05 +0800

    have a api async problem

--- src/app/views/member/member.component.ts
+++ src/app/views/member/member.component.ts
@@ -55,7 +55,7 @@ export class MemberComponent implements OnInit {
     this.isVisible = true;
   }
 
-  handleOk(value: any): void {
+  handleOk(): void {
     console.log(this.validateForm.valid);
     console.log(this.validateForm.controls);
     // tslint:disable-next-line:forin
@@ -75,33 +75,49 @@ export class MemberComponent implements OnInit {
 
   handleCancel(): void {
     console.log('Button cancel clicked!');
+    this.validateForm.reset();
+    // for (const i in this.validateForm.controls) {
+    //   this.validateForm.controls[i].markAsDirty();
+    //   this.validateForm.controls[i].updateValueAndValidity();
+    // }
     this.isVisible = false;
+  }
+
+  submitForm(): void {
+    console.log(this.validateForm.valid);
+    // for (const i in this.validateForm.controls) {
+    //   this.validateForm.controls[i].markAsDirty();
+    //   this.validateForm.controls[i].updateValueAndValidity();
+    // }
   }
 
   phoneAsyncValidator = (control: FormControl) =>
     new Observable((observer: Observer<ValidationErrors | null>) => {
-      setTimeout(() => {
+      setTimeout(async () => {
         const mobile = /^(13[0-9]{9})|(18[0-9]{9})|(14[0-9]{9})|(17[0-9]{9})|(15[0-9]{9})$/
         const tel = /^\d{3,4}-?\d{7,9}$/
         if (mobile.test(control.value) || tel.test(control.value)) {
-          const params = new HttpParams().set('phone', control.value)
-          this.httpService.get('/api/member/checkPhone', {params} )
-            .subscribe((response: any) => {
-              console.log(response);
-              if (response.result) {
-                observer.next({ error: false });
-                this.phoneValidate = 'success';
-                // this.ngOnInit();
-              } else {
-                observer.next(null);
-                this.phoneValidate = 'error';
-              }
-            });
+          const isHave = await this.checkPhone(control.value);
+          console.log(isHave);
+          // setTimeout(() => {
+          //   alert('111' + isHave);
+          isHave === true ? observer.next(null) : observer.next({ error: true, duplicated: true, completed: false });
+          // });
         } else {
-          observer.next(null);
-          this.phoneValidate = 'warning';
+          observer.next({ error: true, duplicated: false, completed: true });
+          // this.phoneValidate = 'warning';
         }
         observer.complete();
       }, 1000);
-    });
+  })
+
+    checkPhone(value: any): any {
+    // const params = new HttpParams().set('phone', value)
+    // this.httpService.get('/api/member/checkPhone', {params} )
+    //   .subscribe((response: any) => {
+    //     console.log(response);
+    //     return response.result;
+    // });
+      return true;
+  }
 }
